b70bd0a373239f75d5ee45ce8ca42a2fe756e1f9
AmritVedPrakash <amritvedprakash76@gmail.com>
2026-08-22 18:29:52 +0530

    Fix formatting by adding a blank line in Qualities component

--- frontend/src/pages/Qualities.jsx
+++ frontend/src/pages/Qualities.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Quality from '../components/qualities/Quality'
 import Training from '../components/qualities/training'
 
+
 export default function Qualities() {
   return (
     <div>
